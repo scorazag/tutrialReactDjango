9a8ef858de4ad9b367b5c5990ac6fb1ec5c13c03
Sergio Coraza <scorazag0@gmail.com>
2020-05-11 18:10:52 -0500

    primer commit

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+import From from './Form'
+import Leads from './Leads'
+
+
+export default function Dashboard(){
+    return(
+        <Fragment>
+            <From />
+            <Leads />
+        </Fragment>
+    )
+}
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export class Leads extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Agregagr un Leads</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads
